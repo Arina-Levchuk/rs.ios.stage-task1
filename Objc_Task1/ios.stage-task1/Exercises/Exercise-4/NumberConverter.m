10012ea6d95c4bd20e92fc75e342b24bacd5e2fb
Arina Levchuk <>
2024-09-09 15:17:14 +0200

    [T1] numberConverter solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,20 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    if (!number) {
+        return @[];
+    }
+    
+    NSString *numberStr = [[number stringValue] stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    
+    NSMutableArray *reversedArray = [NSMutableArray arrayWithCapacity:numberStr.length];
+    
+    for (NSInteger index = numberStr.length - 1; index >= 0; index--) {
+        NSString *charItem = [NSString stringWithFormat:@"%C", [numberStr characterAtIndex:index]];
+        [reversedArray addObject:charItem];
+    }
+    
+    return [reversedArray copy];
 }
 
 @end
