67591e2ebb99f54d0864f628095569875e1a2a0d
Arina Levchuk <>
2024-09-09 16:14:58 +0200

    [T_3] MinRotated solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,28 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    if (self.count == 0) {
+        return nil;
+    }
+    
+    NSInteger left = 0;
+    NSInteger right = self.count - 1;
+    
+    if ([self[left] intValue] <= [self[right] intValue]) {
+        return self[left];
+    }
+    
+    while (left < right) {
+        NSInteger mid = left + (right - left) / 2;
+        
+        if ([self[mid] intValue] > [self[right] intValue]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    
+    return self[left];
 }
 
 @end
